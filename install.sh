d265d0bc59abc97d24c4b0ef702331007e0f28ab
Jan <accounts@galtys.com>
2015-10-14 14:25:28 +0100

    initial import

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install screen graphviz ghostscript postgresql-client \\
+python-dateutil python-feedparser python-matplotlib \\
+python-ldap python-libxslt1 python-lxml python-mako \\
+python-openid python-psycopg2 python-pybabel python-pychart \\
+python-pydot python-pyparsing python-reportlab python-simplejson \\
+python-tz python-vatnumber python-vobject python-webdav \\
+python-werkzeug python-xlwt python-yaml python-imaging
+
+sudo apt-get -y install gcc python-dev mc bzr bzr-fastimport python-setuptools python-babel \\
+python-feedparser python-reportlab-accel python-zsi python-openssl \\
+python-egenix-mxdatetime python-jinja2 python-unittest2 python-mock \\
+python-docutils lptools make python-psutil python-paramiko poppler-utils \\
+python-pdftools antiword postgresql python-pip git python-git
+sudo apt-get -y install emacs sysv-rc-conf aptitude
+sudo apt-get -y install apache2
+sudo apt-get -y install krusader #USER
+
+sudo apt-get -y install libapache2-mod-wsgi nmap
+
+python-pdftools antiword postgresql  git python-git ipython
+sudo apt-get -y install emacs sysv-rc-conf
+sudo apt-get -y install libapache2-mod-wsgi nmap
+
+#sudo pip install pycountry
+#sudo pip install svgutils
+#sudo pip install pycountry
+#sudo pip install svgutils
+#sudo pip install simple-crypt
+#sudo pip install requests
+#sudo pip install pypdf
+#sudo pip install magento  #pymagento
+#sudo pip install requests
+#sudo pip install evdev
+
+
+sudo aptitude -y install openjdk-6-jdk openjdk-6-jre #for pentaho
+sudo aptitude -y install ant
+sudo aptitude -y install openssh-server
+sudo aptitude -y install mdadm
+sudo apt-get -y install python-xlrd python-zbar units
+#zbar-tools ?
+sudo a2enmod ssl
+#wget http://gdata-python-client.googlecode.com/files/gdata-2.0.17.tar.gz 
+#tar zxvf gdata-2.0.17.tar.gz 
+#cd gdata-2.0.17/
+#sudo python setup.py install
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+#sudo su - postgres -s /bin/bash
+sudo apt-get install apt-file
+#sudo pip install passlib
+sudo apt-get -y install python-passlib
+sudo apt-get -y install python-decorator
+#sudo pip install -U git+git://github.com/galtys/codebase-python-api-client
